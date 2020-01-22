135653b5d0cf135265c3ba88e7a2053e4f524eee
kilersunday21 <32273008+kilersunday21@users.noreply.github.com>
2020-01-22 08:44:07 +0530

    Add files via upload

---
+++ P3Bfinal.py
@@ -0,0 +1,35 @@
+print("Name : Anil Chauhan")
+print("Roll No : ##")
+import math
+print("Using 3 inputs 3 weights 1 output.")
+x1=[0.3,0.5,0.8]  #inputs
+w1=[0.1,0.1,0.1]  #weights
+t=1               #TARGET
+a=0.1       #alpha
+diff=1      #initial difference
+yin=0       #initial net input
+
+while(diff>0.4): 
+
+    for i in range(0,3):
+        yin = yin + (x1[i]*w1[i])
+    
+    yin = yin + 0.25   
+    yin=round(yin,3)
+    print("Yin = ",yin)      
+    print("target = ",t)      
+    diff=t-yin
+    
+    diff=round(diff,3)
+    diff=math.fabs(diff)
+    print("error = ",diff)
+    neww1=[]
+    for i in range(0,3):    #update weights
+      w1new=w1[i] + a*diff*x1[i]
+      w1new=round(w1new,2)
+      neww1.append(w1new)
+    print("w1new = ",neww1)
+    w1=neww1
+    print()
+    
+    
